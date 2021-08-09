be9ed93203538fe5e1666a296a7e15a49d625b4d
John-Mark Sackett <jmsackett91@gmail.com>
2021-08-09 18:58:08 -0400

    fixed a typo in the intern class extensuion and wrote the test for intern

---
+++ tests/Intern.test.js
@@ -0,0 +1,26 @@
+const { it, expect, describe } = require('@jest/globals');
+const Intern = require('../lib/Intern');
+
+describe('Intern', () => {
+    it('Should create a new object extension titled intern based off of input.', () => {
+        const intern = new Intern('john', 76, 'johndoe@email.com', 'College University');
+
+        expect(intern.school).toEqual(expect.any(String));
+    })
+});
+
+describe('getSchool', () => {
+    it('Should retrun a string that is the name of the school intern is attending.', () => {
+        const intern = new Intern('john', 76, 'johndoe@email.com', 'College University');
+
+        expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+    })
+});
+
+describe('getRole', () => {
+    it('Should return a role of Intern', () => {
+        const intern = new Intern('john', 76, 'johndoe@email.com', 'College University');
+
+        expect(intern.getRole()).toEqual('Intern');
+    })
+});
